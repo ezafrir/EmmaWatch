6c0bce0938ec05d8090ae8bf41e40dd1118dc8a8
Emma Zafrir <124220741+ezafrir@users.noreply.github.com>
2025-02-19 15:43:20 -0500

    Create sharedProgs.h

---
+++ sharedProgs.h
@@ -0,0 +1,15 @@
+#ifndef SHAREDPROG
+#define SHAREDPROG
+
+#include <Arduino.h>
+
+//void testLights();
+void hotspotConnect();
+extern bool AutoKill;
+extern bool AutoSleep;
+
+extern void updateTimeN(); 
+
+
+
+#endif
